b56f0e5ffccff58b70bfe64a0bbc4b656c5ffced
r.houaoui@gmail.com <r.houaoui@gmail.com>
2018-03-11 14:37:26 +0100

    test() : Adding test with 100% of coverage for Bitcoin folder

--- bitcoin/lib/convert.js
+++ bitcoin/lib/convert.js
@@ -2,67 +2,68 @@
 
 const Big = require('big.js');
 let units = {
-    'BTC': new Big(1),
-    'mBTC': new Big(0.001),
-    'μBTC': new Big(0.000001),
-    'bit': new Big(0.000001),
-    'Satoshi': new Big(0.00000001),
-    'sat': new Big(0.00000001)
+  'BTC': new Big(1),
+  'mBTC': new Big(0.001),
+  'μBTC': new Big(0.000001),
+  'bit': new Big(0.000001),
+  'Satoshi': new Big(0.00000001),
+  'sat': new Big(0.00000001)
 };
 
 function convert(from, fromUnit, toUnit, representation) {
-    let fromFactor = units[fromUnit];
-    if (fromFactor === undefined) {
-        throw new Error(`'${fromUnit}' is not a bitcoin unit`);
-    }
-    let toFactor = units[toUnit];
-    if (toFactor === undefined) {
-        throw new Error(`'${toUnit}' is not a bitcoin unit`);
-    }
+  let fromFactor = units[fromUnit];
+  if (fromFactor === undefined) {
+    throw new Error(`'${fromUnit}' is not a bitcoin unit`);
+  }
+  let toFactor = units[toUnit];
+  if (toFactor === undefined) {
+    throw new Error(`'${toUnit}' is not a bitcoin unit`);
+  }
 
-    if (Number.isNaN(from)) {
-        if (!representation || representation === 'Number') {
-            return from;
-        } else if (representation === 'Big') {
-            return new Big(from); // throws BigError
-        } else if (representation === 'String') {
-            return from.toString();
-        }
-        throw new Error(`'${representation}' is not a valid representation`);
-    }
-
-    let result = new Big(from).times(fromFactor).div(toFactor);
-
+  if (Number.isNaN(from)) {
     if (!representation || representation === 'Number') {
-        return Number(result);
+      return from;
     } else if (representation === 'Big') {
-        return result;
+      return new Big(from); // throws BigError
     } else if (representation === 'String') {
-        return result.toString();
+      return from.toString();
     }
-
     throw new Error(`'${representation}' is not a valid representation`);
+  }
+
+  let result = new Big(from).times(fromFactor).div(toFactor);
+
+  if (!representation || representation === 'Number') {
+    return Number(result);
+  } else if (representation === 'Big') {
+    return result;
+  } else if (representation === 'String') {
+    return result.toString();
+  }
+
+  throw new Error(`'${representation}' is not a valid representation`);
 }
 
-convert.units = function () {
-    return Object.keys(units);
+convert.units = function() {
+  return Object.keys(units);
 };
 
 convert.addUnit = function addUnit(unit, factor) {
-    factor = new Big(factor);
-    let existing = units[unit];
-    if (existing && !existing.eq(factor)) {
-        throw new Error(`'${unit}' already exists with a different conversion factor`);
-    }
-    units[unit] = factor;
+  factor = new Big(factor);
+  let existing = units[unit];
+  if (existing && !existing.eq(factor)) {
+    throw new Error(`'${unit}' already exists with a different conversion factor`);
+  }
+  units[unit] = factor;
 };
 
 let predefinedUnits = convert.units();
 convert.removeUnit = function removeUnit(unit) {
-    if (predefinedUnits.indexOf(unit) >= 0) {
-        throw new Error(`'${unit}' is predefined and cannot be removed`);
-    }
+  if (predefinedUnits.indexOf(unit) >= 0) {
     delete units[unit];
+  } else {
+    throw new Error(`'${unit}' is not predefined and cannot be removed`);
+  }
 };
 
 module.exports = convert;
